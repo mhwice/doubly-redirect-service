935c145159a28f4239440084ec08f06d44c565ba
Marshall Wice <mhwice@protonmail.com>
2025-06-09 15:24:23 -0700

    feat: add consumer logic from cron

--- doubly-queue-consumer/src/index.ts
+++ doubly-queue-consumer/src/index.ts
@@ -1,10 +1,84 @@
+import { neon } from '@neondatabase/serverless';
+import { PayloadSchema } from "./schema";
+
 export interface Env {
   QUEUE: Queue<any>;
+  DATABASE_URL: string;
 }
 
 export default {
   async queue(batch, env): Promise<void> {
-    let messages = JSON.stringify(batch.messages);
-    console.log(`consumed from our queue: ${messages}`);
+
+    const sql = neon(env.DATABASE_URL);
+
+    // validate messages, discard bad ones
+    const validatedMessages = [];
+    for (const message of batch.messages) {
+      const payload = message.body;
+      const validated = PayloadSchema.safeParse(payload);
+      if (!validated.success) continue;
+      validatedMessages.push(validated.data);
+    }
+
+    if (validatedMessages.length === 0) return;
+
+    const keys = Array.from(Object.keys(validatedMessages[0]));
+    const perRow = keys.length;
+    const placeholders = validatedMessages.map((_, rowIdx) => {
+      const start = rowIdx * perRow + 1;
+      const placeholders = Array(perRow).fill(null).map((_, i) => `$${start + i}`)
+      return `(${placeholders.join(', ')})`;
+    }).join(', ');
+
+    const query = `
+      INSERT INTO click_events (${keys})
+      VALUES ${placeholders}
+      RETURNING *;
+    `;
+
+    const values = validatedMessages.flatMap(msg =>
+      // for each message, pull out its columns in the same order as `keys`
+      keys.map(key => (msg as Record<string, unknown>)[key])
+    );
+
+    const response = await sql(query, values);
+    console.log(`response: ${JSON.stringify(response)}`);
   },
 } satisfies ExportedHandler<Env>;
+
+/*
+
+linkId:
+
+123,
+ua:
+
+"Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:128.0) Gecko/20100101 Firefox/128.0",
+longitude:
+
+"-123.68596",
+latitude:
+
+"48.84133",
+city:
+
+"North Cowichan",
+region:
+
+"BC",
+country:
+
+"CA",
+continent:
+
+"NA",
+url:
+
+"https://doubly-queue-producer.redis-emphatic073.workers.dev/6iqHtcKDQZ3V",
+createdAt:
+
+"2025-06-09T21:31:19.743Z",
+
+
+
+*/
---
+++ doubly-queue-consumer/src/schema.ts
@@ -0,0 +1,90 @@
+import { UAParser } from "ua-parser-js";
+import { z } from "zod";
+import iso3166 from "iso-3166-2";
+
+export const PayloadSchema = z.object({
+  linkId: z.number(),
+  createdAt: z.coerce.date(),
+  ua: z.string().optional(),
+  latitude: z.string().optional(),
+  longitude: z.string().optional(),
+  city: z.string().optional(),
+  region: z.string().optional(),
+  country: z.string().optional(),
+  continent: z.string().optional(),
+  url: z.string(),
+}).transform((obj) => {
+
+  const { linkId, createdAt, ua, latitude, longitude, city, region, country, continent, url } = obj;
+
+  // extract from ua
+  let browser, os, device;
+  try {
+    const parser = new UAParser(ua);
+    const uaResult = parser.getResult();
+    browser = uaResult.browser.name;
+    os = uaResult.os.name;
+    device = uaResult.device.type;
+  } catch (error) {
+
+  }
+
+  const parsedUrl = new URL(url);
+  const source = parsedUrl.searchParams.get("source");
+
+  let parsedRegion = undefined;
+    if (country && region) {
+      // converts 'BC' to 'British Columbia'
+      parsedRegion = iso3166.subdivision(country, region)?.name;
+    }
+
+    let parsedCountry = undefined;
+    if (country) {
+      // converts 'CA' to 'Canada'
+      parsedCountry = iso3166.country(country)?.name;
+    }
+
+    let parsedContinent = undefined;
+    if (continent) {
+      switch (continent) {
+        case 'NA':
+          parsedContinent = 'North America';
+          break;
+        case 'EU':
+          parsedContinent = 'Europe';
+          break;
+        case 'AF':
+          parsedContinent = 'Africa';
+          break;
+        case 'AS':
+          parsedContinent = 'Asia';
+          break;
+        case 'SA':
+          parsedContinent = 'South America';
+          break;
+        case 'OC':
+          parsedContinent = 'Oceania';
+          break;
+        case 'AN':
+          parsedContinent = 'Antarctica';
+          break;
+        default:
+          break;
+      }
+    }
+
+  return {
+    id: linkId,
+    createdAt,
+    source: source === "qr" ? "qr" : "link",
+    latitude: latitude === undefined ? undefined : parseFloat(latitude),
+    longitude: longitude === undefined ? undefined : parseFloat(longitude),
+    city: city || "unknown",
+    region: parsedRegion || "unknown",
+    country: parsedCountry || "unknown",
+    continent: parsedContinent || "unknown",
+    browser: browser || "unknown",
+    os: os || "unknown",
+    device: device || "unknown"
+  }
+});
